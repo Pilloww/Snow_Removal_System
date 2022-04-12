ea9de5363f3266385d9dd3c4feb6525b30576ff6
Darien Lee <43325791+Pilloww@users.noreply.github.com>
2022-04-12 19:13:20 -0400

    Add files via upload

--- server.py
+++ server.py
@@ -11,11 +11,12 @@ channel = connection.channel()
 
 channel.queue_declare(queue='rpc_queue')
 dicts = utility.create_dictionaries()
+
 def on_request(ch, method, props, body):
-    
+        
     n = body.decode()
     nput = n.split(', ')
-    if(weather('Toronto', 'CA').weather == "Snow"):
+    if(weather('Toronto', 'CA').weather != "Snow"):
         if(nput[0] == 'driver'):
             print("[.] Received requests from 'driver'")
             response = dicts[0][nput[1]]
@@ -26,6 +27,7 @@ def on_request(ch, method, props, body):
                                                                  props.correlation_id),
                              body=str(response))
             ch.basic_ack(delivery_tag=method.delivery_tag)
+            print("[x] Sent response to 'driver'")
     
         elif(nput[0] == 'client'):
             print("[.] Received requests from 'client'")
@@ -37,11 +39,12 @@ def on_request(ch, method, props, body):
                                                                  props.correlation_id),
                              body=str(response))
             ch.basic_ack(delivery_tag=method.delivery_tag)
+            print("[x] Sent response to 'client'")
     else:
         response = "No vehicles are being dispatched since it is not snowing"
 
 channel.basic_qos(prefetch_count=1)
 channel.basic_consume(queue='rpc_queue', on_message_callback=on_request)
 
-print(" [x] Awaiting RPC requests")
+print("[x] Awaiting RPC requests")
 channel.start_consuming()
